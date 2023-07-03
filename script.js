f84890413ac20626b1eff8707368512a6c4d2e3d
Mangesh Dushman <mangeshdushman49@gmail.com>
2023-07-03 14:03:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+// let weather = {
+//     apiKey: "67b92f0af5416edbfe58458f502b0a31",
+//     fetchWeather: function(city) {
+//         fetch(
+//                 "https://api.openweathermap.org/data/2.5/weather?q=" +
+//                 city +
+//                 "&units=metric&appid=" +
+//                 this.apiKey
+//             )
+//             .then((response) => {
+//                 if (!response.ok) {
+//                     alert("No weather found.");
+//                     throw new Error("No weather found.");
+//                 }
+//                 return response.json();
+//             })
+//             .then((data) => this.displayWeather(data));
+//     },
+//     displayWeather: function(data) {
+//         const { name } = data;
+//         const { icon, description } = data.weather[0];
+//         const { temp, humidity } = data.main;
+//         const { speed } = data.wind;
+//         document.querySelector(".city").innerText = "Weather in " + name;
+//         document.querySelector(".icon").src =
+//             "https://openweathermap.org/img/wn/" + icon + ".png";
+//         document.querySelector(".description").innerText = description;
+//         document.querySelector(".temp").innerText = temp + "°C";
+//         document.querySelector(".humidity").innerText =
+//             "Humidity: " + humidity + "%";
+//         document.querySelector(".wind").innerText =
+//             "Wind speed: " + speed + " km/h";
+//         document.querySelector(".weather").classList.remove("loading");
+//         document.body.style.backgroundImage =
+//             "url('https://source.unsplash.com/1600x900/?" + name + "')";
+//     },
+//     search: function() {
+//         this.fetchWeather(document.querySelector(".search-bar").value);
+//     },
+// };
+
+// document.querySelector(".search button").addEventListener("click", function() {
+//     weather.search();
+// });
+
+// document
+//     .querySelector(".search-bar")
+//     .addEventListener("keyup", function(event) {
+//         if (event.key == "Enter") {
+//             weather.search();
+//         }
+//     });
+
+// weather.fetchWeather("Patna");
+
+let weather = {
+    apiKey: "67b92f0af5416edbfe58458f502b0a31",
+    fetchWeather: function(city) {
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+            )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+            "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+    searchByLocation: function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    fetch(
+                            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.apiKey}`
+                        )
+                        .then((response) => {
+                            if (!response.ok) {
+                                alert("No weather found.");
+                                throw new Error("No weather found.");
+                            }
+                            return response.json();
+                        })
+                        .then((data) => this.displayWeather(data));
+                },
+                (error) => {
+                    console.log(error);
+                    alert("Unable to retrieve your location. Please enter a city manually.");
+                }
+            );
+        } else {
+            alert(
+                "Geolocation is not supported by this browser. Please enter a city manually."
+            );
+        }
+    }
+};
+
+function searchWeather() {
+    weather.search();
+}
+
+function searchByLocation() {
+    weather.searchByLocation();
+}
+
+document.querySelector(".search-bar").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        weather.search();
+    }
+});
+
+window.addEventListener("load", () => {
+    weather.searchByLocation();
+});
